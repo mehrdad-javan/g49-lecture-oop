474fd5648c952baf831a70542f91c669ff3c0d46
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-03-26 09:08:35 +0100

    add class diagram for part 2

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -9,7 +9,6 @@ public class App {
         Fries defaultSmallFries = new Fries();
         Burger cheeseBurger = new Burger("Cheese Burger", 100, "CheeseBurger", true);
 
-       // how to call addItem method of OrderItem  class ->  ?
         OrderItem orderItem = new OrderItem();
         orderItem.addItem(largeFries);
         orderItem.addItem(defaultSmallFries);
